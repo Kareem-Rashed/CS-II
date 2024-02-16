9b82a8055107bd4854b7639e34f7fd15b8f3e238
KareemRashed <Kareemrashed@aucegypt.edu>
2024-02-16 17:22:12 +0200

    .

---
+++ CS II/CS II/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS II
+//
+//  Created by Kareem Rashed on 16/02/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
